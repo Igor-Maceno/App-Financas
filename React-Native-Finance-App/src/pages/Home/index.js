dc2742925fce7fe7f22410efb1829aa23160eaad
Igor-Maceno <igorlopesmaceno17@gmail.com>
2024-04-28 16:46:05 -0300

    Balance

--- React-Native-Finance-App/src/pages/Home/index.js
+++ React-Native-Finance-App/src/pages/Home/index.js
@@ -1,13 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 //components
-import Header from '../../components/Header';
+import Header from "../../components/Header";
+import Balance from "../../components/Balance";
+
+const list = [
+  {
+    id: 1,
+    label: "MhNet",
+    value: "99,99",
+    date: "10/04/2024",
+    type: 0, //despesas,
+  },
+  {
+    id: 2,
+    label: "Banco Inter",
+    value: "57,89",
+    date: "10/04/2024",
+    type: 0, //despesas,
+  },
+  {
+    id: 3,
+    label: "Pix Natalia",
+    value: "200",
+    date: "07/04/2024",
+    type: 1, //receita,
+  },
+];
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Header name="Igor Maceno"/>
-      <Text>App Finanças</Text>
+      <Header name="Igor Maceno" />
+      <Balance balance="16.527,95" expenses="-434,89" />
+      <Text style={styles.title}>Últimas movimentações</Text>
+      <FlatList
+        style={styles.list}
+        data={list}
+        keyExtractor={(item) => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => <Text>TESTE</Text>}
+      />
     </View>
   );
 }
@@ -15,6 +48,17 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginLeft: 16,
+    marginRight: 14,
+    marginTop: 14,
+  },
+  list: {
+    marginStart: 14,
+    marginTop: 14,
   },
 });
